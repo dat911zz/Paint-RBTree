7d290099c095d554ce59df3a63d78d95079f2833
dat911zz <datcy2011@gmail.com>
2021-12-03 21:05:58 +0700

    update 9:05 PM 3/12/2021

---
+++ 2001202045-VuNgoDat-THCTDL-Buoi7/Dictionary.h
@@ -0,0 +1,19 @@
+#ifndef _DICTIONARY_H_
+#define _DICTIONARY_H_
+//Struct and Constant
+struct Dictionary
+{
+	char word[100];
+	char mean[100];
+};
+//Functions
+bool operator > (Dictionary a, Dictionary b)
+{
+	return strcmp(a.word,b.word) == 1;
+}
+bool operator < (Dictionary a, Dictionary b)
+{
+	return strcmp(a.word, b.word) == -1;
+}
+
+#endif // !_DICTIONARY_H_
--- 2001202045-VuNgoDat-THCTDL-Buoi7/Source.cpp
+++ 2001202045-VuNgoDat-THCTDL-Buoi7/Source.cpp
@@ -4,14 +4,18 @@
 #include<string>
 #include"PhanSo.h"
 #include"BRTree.h"
+#include"Dictionary.h"
+#include"PrintDictionaryTree.h"
 #include"PrintTree.h"
 using namespace std;
 //Struct and Constant
 
 //Prototype
+void readFilePS(BRTree<PhanSo>& tree);
 void showPS(BRTNode<PhanSo>* p);
 void showPS_2(PhanSo x);
 void menu();
+void menu_2();
 void traversePS(BRTree<PhanSo> tree);
 void NLR_BFS(BRTNode<PhanSo>* root);
 void NRL_BFS(BRTNode<PhanSo>* root);
@@ -35,124 +39,200 @@ void xuatPSMauLaSCP(BRTNode<PhanSo>* root);
 bool isPerfectNum(int n);
 void xuatPSTuVaMauLaSoHoanHao(BRTNode<PhanSo>* root);
 
+void readFileDictionary(BRTree<Dictionary>& tree, string filename);
+void showDictionaryNode(BRTNode<Dictionary>* root);
+
+
 //Main
 void main()
 {
 	PhanSo a[100] = { {1,3},{2,4},{2,5},{9,4},{9,2},{5,5},{3,4},{75,43},{5,7},{2,3} };
-	//PhanSo a[100] = { {1,2} };
 	int n = 10;
 	BRTree<PhanSo> tree;
+	BRTree<Dictionary> dictionary;
 	initBRTree(tree);
+	initBRTree(dictionary);
 	int chon;
-	do
+	cout << "1- Phan so\t2- Tu dien" << endl;
+	cout << "Chon: ";
+	cin >> chon;
+	switch (chon)
 	{
-		system("cls");
-		menu();
-		print_ascii_tree(tree.Root);
-		set_Color(0, 7);
-		cout << "==========================================================================" << endl;
-		cout << "Chon: ";
-		cin >> chon;
-		
-		system("cls");
-		print_ascii_tree(tree.Root);
-		set_Color(0, 7);
-		switch (chon)
+	case 1:
+		do
 		{
-		case 1:
-			for (int i = 0; i < n; i++)
+			system("cls");
+			menu();
+			print_ascii_tree(tree.Root);
+			set_Color(0, 7);
+			cout << "==========================================================================" << endl;
+			cout << "Chon: ";
+			cin >> chon;
+
+			system("cls");
+			print_ascii_tree(tree.Root);
+			set_Color(0, 7);
+			switch (chon)
 			{
-				toiGianPS(a[i]);
-				if (findBRTNode(tree.Root, a[i]) == NULL)
+			case 1:
+				for (int i = 0; i < n; i++)
 				{
-					insert(tree.Root, a[i]);
+					toiGianPS(a[i]);
+					if (findBRTNode(tree.Root, a[i]) == NULL)
+					{
+						insert(tree.Root, a[i]);
+					}
 				}
+				showBRTree_InOrder(tree.Root, showPS);
+				break;
+			case 2:
+				readFilePS(tree);
+				break;
+			case 3:
+				traversePS(tree);
+				break;
+			case 4://Chieu rong
+				NLR_BFS(tree.Root);
+				cout << endl;
+				NRL_BFS(tree.Root);
+				cout << endl;
+				break;
+			case 5:
+				cout << "LNR: ";
+				LNR_DFS(tree.Root);
+				cout << endl;
+				cout << "RNL: ";
+				RNL_DFS(tree.Root);
+				cout << endl;
+				cout << "NLR: ";
+				NLR_DFS(tree.Root);
+				cout << endl;
+				cout << "NRL: ";
+				NRL_DFS(tree.Root);
+				cout << endl;
+				break;
+			case 6:
+				insertX(tree);
+				break;
+			case 7:
+				findX(tree);
+				break;
+			case 8:
+				removeX(tree);
+				print_ascii_tree(tree.Root);
+				set_Color(0, 7);
+				break;
+			case 9:
+				removeElementsGreaterThan2(tree.Root);
+				break;
+			case 10:
+				removeDenominatorPrimeElements(tree.Root);
+				print_ascii_tree(tree.Root);
+				set_Color(0, 7);
+				break;
+			case 11:
+				cout << "\nTong phan so trong cay: " << sumOfFractions(tree.Root);
+				cout << "\nTong phan so trong cay(phan so): ";
+				PhanSo sum = sumOfFractions_2(tree.Root);
+				toiGianPS(sum);
+				showPS_2(sum);
+				break;
+			case 12:
+				int k;
+				cout << "\nNhap muc k: ";
+				cin >> k;
+				cout << "\nSo luong phan so muc k: " << countFractionsLevelK(tree.Root, k);
+				break;
+			case 13:
+				deleteBRTree(tree.Root);
+				break;
+			case 14:
+				cout << "\nXuat PS co tu nho hon mau: ";
+				xuatPSTuNhoHonMau(tree.Root);
+				break;
+			case 15:
+				cout << "\nXuat PS co mau so la so chinh phuong: ";
+				xuatPSMauLaSCP(tree.Root);
+				break;
+			case 16:
+				cout << "\nXuat PS co tu so va mau so dong thoi la so hoan thien: ";
+				xuatPSTuVaMauLaSoHoanHao(tree.Root);
+				break;
+			default:
+				cout << "EXIT!" << endl;
+				break;
 			}
-			showBRTree_InOrder(tree.Root, showPS);
-			break;
-		case 2:
-			break;
-		case 3:
-			traversePS(tree);
-			break;
-		case 4://Chieu rong
-			NLR_BFS(tree.Root);
 			cout << endl;
-			NRL_BFS(tree.Root);
-			cout << endl;
-			break;
-		case 5:
-			cout << "LNR: ";
-			LNR_DFS(tree.Root);
-			cout << endl;
-			cout << "RNL: ";
-			RNL_DFS(tree.Root);
-			cout << endl;
-			cout << "NLR: ";
-			NLR_DFS(tree.Root);
-			cout << endl;
-			cout << "NRL: ";
-			NRL_DFS(tree.Root);
-			cout << endl;
-			break;
-		case 6:
-			insertX(tree);
-			break;
-		case 7:
-			findX(tree);
-			break;
-		case 8:		
-			removeX(tree);
+			set_Color(0, 7);
+			system("pause");
+		} while (chon > 0 && chon <= 16);
+		break;
+	case 2:
+		do
+		{
+			system("cls");
+			menu_2();
 			print_ascii_tree(tree.Root);
 			set_Color(0, 7);
-			break;
-		case 9:
-			removeElementsGreaterThan2(tree.Root);
-			break;
-		case 10:
-			removeDenominatorPrimeElements(tree.Root);
+			cout << "==========================================================================" << endl;
+			cout << "Chon: ";
+			cin >> chon;
+
+			system("cls");
 			print_ascii_tree(tree.Root);
 			set_Color(0, 7);
-			break;
-		case 11:
-			cout << "\nTong phan so trong cay: " << sumOfFractions(tree.Root);
-			cout << "\nTong phan so trong cay(phan so): "; 
-			PhanSo sum = sumOfFractions_2(tree.Root);
-			toiGianPS(sum);
-			showPS_2(sum);
-			break;
-		case 12:
-			int k;
-			cout << "\nNhap muc k: ";
-			cin >> k;
-			cout << "\nSo luong phan so muc k: " << countFractionsLevelK(tree.Root, k);
-			break;
-		case 13:
-			deleteBRTree(tree.Root);
-			break;
-		case 14:
-			cout << "\nXuat PS co tu nho hon mau: ";
-			xuatPSTuNhoHonMau(tree.Root);
-			break;
-		case 15:
-			cout << "\nXuat PS co mau so la so chinh phuong: ";
-			xuatPSMauLaSCP(tree.Root);
-			break;
-		case 16:
-			cout << "\nXuat PS co tu so va mau so dong thoi la so hoan thien: ";
-			xuatPSTuVaMauLaSoHoanHao(tree.Root);
-			break;
-		default:
-			cout << "EXIT!" << endl;
-			break;
-		}
-		cout << endl;
-		set_Color(0, 7);
-		system("pause");
-	} while (chon > 0 && chon <= 15);
+			switch (chon)
+			{
+			case 1:
+				readFileDictionary(dictionary, "Dictionary.txt");
+				print_ascii_tree_2(dictionary.Root);
+				cout << "\nLNR: ";
+				LNR(dictionary.Root, showDictionaryNode);
+				break;
+			case 2:
+				print_ascii_tree_2(dictionary.Root);
+				cout << "\nBFS: ";
+				levelOrder(dictionary.Root, showDictionaryNode);
+				cout << "\nDFS: ";
+				iterativePreOrder(dictionary.Root, showDictionaryNode);
+				break;
+			case 3:
+
+				break;
+			case 4:
+				break;
+			default:
+				cout << "EXIT!" << endl;
+				break;
+			}
+			cout << endl;
+			system("pause");
+		} while (chon > 0 && chon <= 9);
+		break;
+	}
+	
 }
 
 //Functions
+void readFilePS(BRTree<PhanSo>& tree)
+{
+	ifstream in;
+	string filename = "PhanSo.txt";
+	if (in)
+	{
+		while (!in.eof())
+		{
+			PhanSo tmp;
+			in >> tmp.tu >> tmp.mau;
+			toiGianPS(tmp);
+			if (findBRTNode(tree.Root, tmp) == NULL)
+			{
+				insert(tree.Root, tmp);
+			}
+		}
+	}
+	
+}
 void showPS(BRTNode<PhanSo>* p)
 {
 	if (p->Color == RED)
@@ -172,6 +252,23 @@ void menu()
 {
 	ifstream in;
 	string filename = "menu.txt";
+	SetConsoleOutputCP(65001);
+	in.open(filename);
+	if (in)
+	{
+		while (!in.eof())
+		{
+			string line;
+			getline(in, line);
+			cout << line << endl;
+		}
+	}
+	in.close();
+}
+void menu_2()
+{
+	ifstream in;
+	string filename = "menu_2.txt";
 	SetConsoleOutputCP(65001);
 	in.open(filename);
 	if (in)
@@ -513,4 +610,45 @@ void xuatPSTuVaMauLaSoHoanHao(BRTNode<PhanSo>* root)
 	xuatPSTuVaMauLaSoHoanHao(root->Left);
 	xuatPSTuVaMauLaSoHoanHao(root->Right);
 }
+//============================================
 //Bai 4
+void readFileDictionary(BRTree<Dictionary>& tree, string filename)
+{
+	ifstream in;
+	in.open(filename);
+	if (in)
+	{
+		initBRTree(tree);
+		Dictionary tmp;
+		while (!in.eof())
+		{
+			in >> tmp.word >> tmp.mean;
+			insert(tree.Root, tmp);
+		}
+	}
+}
+void showDictionaryNode(BRTNode<Dictionary>* root)
+{
+	printf(" [%s|%s]", root->Info.word, root->Info.mean);
+}
+BRTNode<Dictionary>* searchMeanOfWord(BRTNode<Dictionary>* root, const char* x)
+{
+	if (root == NULL)
+	{
+		return NULL;
+	}
+
+	if (root->Info.mean == x)
+	{
+		return root;
+	}
+	else
+	{
+		if (true)
+		{
+
+		}
+	}
+	searchMeanOfWord(root->Left, x);
+	searchMeanOfWord(root->Right, x);
+}
